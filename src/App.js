59a3019094bdc2e8f320aae1cf3a370b6e8580cb
chandev0908 <chandev0908@gmail.com>
2021-06-05 19:27:29 +0800

    adjust the context api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
+import { HomepageContext } from "./Context/HomepageContext";
 import "./index.css";
 import Homepage from "./pages/HomePage";
-import SearchResults from "./pages/SearchResults";
 import Preview from "./pages/Preview";
+import SearchResults from "./pages/SearchResults";
+
 function App() {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/search/:query" component={SearchResults} />
-        <Route exact path="/anime/:id" component={Preview} />
-      </Switch>
-    </div>
+    <HomepageContext.Provider value={{ toggleSidebar, setToggleSidebar }}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/search/:query" component={SearchResults} />
+          <Route exact path="/anime/:id" component={Preview} />
+        </Switch>
+      </div>
+    </HomepageContext.Provider>
   );
 }
 export default App;
