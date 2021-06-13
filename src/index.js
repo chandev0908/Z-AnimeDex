6827fc212f70e21664cb41044c9feafc6ca4a9b0
chandev0908 <chandev0908@gmail.com>
2021-06-13 17:10:19 +0800

    remove browserrouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter as Router , HashRouter} from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
