bdc9e963ce6e5366b5fae7724c49fe5ed8875655
chandev0908 <chandev0908@gmail.com>
2021-06-21 18:38:38 +0800

    change tags and added handler for loading

--- src/components/Homepage/SectionTwo.jsx
+++ src/components/Homepage/SectionTwo.jsx
@@ -3,28 +3,39 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import fetchTopAnime from "../../redux/action/topAnimeAction";
 import fetchAnimeSeason from "../../redux/action/animeSeasonAction";
-import fetchTopManga from "../../redux/action/topMangaAction";
 import AnimeRow from "./AnimeRow";
+import { createLoadingSelector } from "../../redux/reducer/selectors";
+import {
+  FETCH_TOP_ANIME_SUCCESS,
+  FETCH_TOP_ANIME_REQUEST,
+  FETCH_SEASON_ANIME_SUCCESS,
+  FETCH_SEASON_ANIME_REQUEST,
+} from "../../redux/types";
 
 function SectionTwo({ animes, actionCreators }) {
   useEffect(() => {
     actionCreators.fetchTopAnime();
     actionCreators.fetchAnimeSeason();
-    actionCreators.fetchTopManga();
   }, [actionCreators]);
   return (
-    <div className="section-two w-full h-auto bg-secondary pt-6 pb-6">
-      <AnimeRow category="Top Anime" animes={animes?.topAnime} />
-      <AnimeRow category="Season Now" animes={animes?.animeSeason} />
-    </div>
+    <section className="section-two w-full h-auto bg-secondary pt-6 pb-6">
+      <AnimeRow category="Top Anime" animes={animes?.topAnime} loader={animes?.loadingReducer.GET_FETCH_TOPS_ANIME}/>
+      <AnimeRow category="Season Now" animes={animes?.animeSeason} loader={animes?.loadingReducer.GET_FETCH_SEASON_ANIME}/>
+    </section>
   );
 }
+const loadingSelector = createLoadingSelector([
+  FETCH_TOP_ANIME_SUCCESS,
+  FETCH_TOP_ANIME_REQUEST,
+  FETCH_SEASON_ANIME_SUCCESS,
+  FETCH_SEASON_ANIME_REQUEST,
+]);
 const mapStateToProps = (state) => {
   return {
     animes: {
       topAnime: state.topAnime.items,
       animeSeason: state.animeSeason.items,
-      topManga: state.topManga.items
+      loadingReducer: state.loadingReducer
     },
   };
 };
@@ -34,7 +45,6 @@ function mapDispatchToProps(dispatch) {
       {
         fetchTopAnime,
         fetchAnimeSeason,
-        fetchTopManga
       },
       dispatch
     ),
